3641c9db87b121bc382a1e3ec0262fc63c640bb5
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2026-05-15 11:49:32 -0400

    feat: by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,50 +5,57 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.send(JSON.stringify(books,null,4))
+public_users.get('/', function (req, res) {
+    //Write your code here
+    return res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
-//   return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author;
-  let matchedBooks = {};
-  const keys = Object.keys(books);
-
-  keys.forEach((key)=>{
-    if (books[key].author === author) {
-        matchedBooks[key] = books[key];
-    }
+public_users.get('/isbn/:isbn', function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+    //   return res.status(300).json({message: "Yet to be implemented"});
 });
-res.json(matchedBooks)
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+    //Write your code here
+    const author = req.params.author;
+    let matchedBooks = {};
+    const keys = Object.keys(books);
+
+    keys.forEach((key) => {
+        if (books[key].author === author) {
+            matchedBooks[key] = books[key];
+        }
+    });
+    res.json(matchedBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    let matchedTitles = {};
+    const keys = Object.keys(books);
+    keys.forEach((key)=>{
+        if (books[key].title === title){
+            matchedTitles[key] = books[key];
+        }
+    });
+    res.json(matchedTitles);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
