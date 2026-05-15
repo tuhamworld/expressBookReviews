98060e8101445d78bd998d565dfc94c6c1d756f8
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2026-05-15 11:41:40 -0400

    feat: getbooks by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let matchedBooks = {};
+  const keys = Object.keys(books);
+
+  keys.forEach((key)=>{
+    if (books[key].author === author) {
+        matchedBooks[key] = books[key];
+    }
+});
+res.json(matchedBooks)
 });
 
 // Get all books based on title
