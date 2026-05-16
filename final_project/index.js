6b6ed442c9bbcc3e62bed6d2249ba170c7d37b7e
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2026-05-16 13:34:38 -0400

    feat: customer login

--- final_project/index.js
+++ final_project/index.js
@@ -8,34 +8,33 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-// Auth mechanism starts here
-  // Check if user is logged in and has valid access token
-  if (req.session.authorization) {
-    let token = req.session.authorization['accessToken'];
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Auth mechanism starts here
+    // Check if user is logged in and has valid access token
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
 
-    // Verify JWT token
-    jwt.verify(token, "access", (err, user) => {
-        if (!err) {
-            req.user = user;
-            next(); // Proceed to the next middleware
-        } else {
-            return res.status(403).json({ message: "User not authenticated" });
-        }
-    });
-} else {
-    return res.status(403).json({ message: "User not logged in" });
-}
-// Auth mechanism ends here
-
-
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+    // Auth mechanism ends here
 });
- 
-const PORT =5000;
+
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
