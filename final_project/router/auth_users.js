519d6cea158f42b14e52fa8753b4ca25a7953c0a
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2026-05-16 15:18:20 -0400

    feat: added review for customers

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,8 +49,22 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.username;
+    // Check if books exist
+    if (!books[isbn]){
+        return res.status(404).json({
+            message: "Book not found"
+        });
+    }
+    // Add or update review
+    books[isbn].reviews[username] = review;
+    return res.status(200).json({
+        message: "Review added/updated successfully",
+        reviews: books[isbn].reviews
+    })
+   
 });
 
 module.exports.authenticated = regd_users;
